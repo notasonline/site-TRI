356d3b5c5ac53c07c737a78c6133425d5b05442e
studyflow <fabricio.tal2233@gmail.com>
2025-08-26 22:19:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-
+[
+    {
+        "matricula": "2025001",
+        "aluno": "ANA CLARA PEREIRA",
+        "LC": 8.5,
+        "CH": 7.8,
+        "CN": 9.0,
+        "MT": 8.2
+    },
+    {
+        "matricula": "2025002",
+        "aluno": "BRUNO COSTA SANTOS",
+        "LC": 6.5,
+        "CH": 9.2,
+        "CN": 7.1,
+        "MT": 7.5
+    },
+    {
+        "matricula": "2025003",
+        "aluno": "CARLOS EDUARDO LIMA",
+        "LC": 9.0,
+        "CH": 8.8,
+        "CN": 8.5,
+        "MT": 9.5
+    }
+]
